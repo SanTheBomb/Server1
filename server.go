3d4a3fa6f8c392508f802f00dd3ddeab300330ce
SanTheBomb <109622435+SanTheBomb@users.noreply.github.com>
2023-06-08 15:39:51 -0500

    Fixed some typos

--- server.go
+++ server.go
@@ -12,8 +12,8 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	print("Please refer to https://blog.logrocket.com/creating-a-web-server-with-golang/")
-	print("If Windows Defender Firewall prompts appear, allow only on private networks for security")
+	print("Please refer to https://blog.logrocket.com/creating-a-web-server-with-golang/. ")
+	print("If Windows Defender Firewall prompts appear, allow only on private networks for security. ")
 
 	fmt.Printf("Starting Server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
